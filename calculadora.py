4f321da122a05c1e6896507b98493f3a94bda904
bielfp <gabrielpecanha14@gmail.com>
2026-04-10 14:09:47 -0300

    adiciona tipo de calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,17 +1,24 @@
+import math
+
 num1 = float(input('Digite o número: '))
-operacao = str(input('Qual operação deseja fazer? (+. -, x, /): '))
+tipo_calculadora = str(input('Qual tipo de calculadora deseja usar? (Simples ou Científica): '))
+operacao_simples = str(input('Qual operação deseja fazer? (+, -, x, /): '))
 num2 = float(input('Digite o outro número: '))
 
-if operacao== '+':
-    result = num1 + num2 
+if tipo_calculadora == 'Simples' or tipo_calculadora == 'simples':
+    if operacao_simples == '+':
+        result = num1 + num2 
 
-if operacao== '-':
-    result = num1 - num2 
+    if operacao_simples == '-':
+        result = num1 - num2 
 
-if operacao== '/':
-    result = num1 / num2 
+    if operacao_simples == '/':
+        result = num1 / num2 
 
-if operacao== 'x':
-    result = num1 * num2 
+    if operacao_simples == 'x':
+        result = num1 * num2 
 
-print(f'{num1:.0f} {operacao} {num2:.0f} = {result:.0f}')
+    print(f'{num1:.0f} {operacao_simples} {num2:.0f} = {result:.0f}')
+
+elif tipo_calculadora == 'Científica' or tipo_calculadora == 'científica' or tipo_calculadora == 'cientifica' or tipo_calculadora == 'Cientifica':
+    pass
